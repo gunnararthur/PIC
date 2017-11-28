a79f74f2767f8c05782dcce195ec3c1fae67b2ed
Gunnar Arthúr Helgason <gunnararthur@Helgis-Air.lan>
2017-11-28 16:31:31 +0100

    November 28, 2017 – Fækkun og endurskipulagning view-a í 'skraning'

--- skraning/urls.py
+++ skraning/urls.py
@@ -6,20 +6,13 @@ from . import views
 app_name = 'skraning'
 urlpatterns = [
     # ex: /skraning/
-    url(r'^$', views.contact_info, name='contact_info'),
-
-    url(r'^to_student_info/', views.to_student_info, name='to_student_info'),
-    # ex: /skraning/45/53
-    url(r'^(?P<contact_id>[0-9]+)/(?P<group_id>[0-9]+)/(?P<message>[0-9]+)$', views.student_info, name='student_info'),
+    url(r'^(?P<message>[0-9]*)$', views.enrollment_info, name='enrollment_info'),
 
     url(r'^download_excel/$', views.download_excel, name='download_excel'),
 
-    url(r'^read_excel/(?P<contact_id>[0-9]+)/(?P<group_id>[0-9]+)$', views.read_excel_upload, name='read_excel_upload'),
+    url(r'^upload_enrollment_info/', views.upload_enrollment_info, name='upload_enrollment_info'),
 
-    url(r'^next_student_info/(?P<contact_id>[0-9]+)/(?P<group_id>[0-9]+)$', views.next_student_info, name='next_student_info'),
+    url(r'^(?P<contact_id>[0-9]+)/(?P<group_id>[0-9]+)/$', views.finish_enrollment, name='finish_enrollment'),
 
-    url(r'^(?P<contact_id>[0-9]+)/(?P<group_id>[0-9]+)/tafla$', views.student_table, name='student_table'),
-
-    url(r'finish', views.finish_enrollment, name='finish_enrollment'),
 
 ]
--- skraning/views.py
+++ skraning/views.py
@@ -1,65 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
-
 from .models import Group, Student, Contact, Round
 import os, pandas
 
 # Create your views here.
 
-def contact_info(request):
-    return render(request, 'skraning/contact_info.html')
-
-def to_student_info(request):
-    # Bý til contact sem á að bæta við
-    # ÞARF AÐ ATHUGA MEÐ TVÍRIT
-    contact = Contact(name=request.POST['name'], email=request.POST['email'])
-    contact.save()
-
-    # Bý til group sem á að bæta við og tengja við added_contact
-    # ÞARF AÐ ATHUGA MEÐ TVÍRIT
-    group = Group(school=request.POST['school'], grade=request.POST['grade'])
-    group.save()
-
-    contact.groups.add(group)
-
-    return HttpResponseRedirect(reverse('skraning:student_info', args=[contact.id, group.id,'0']))
-
-def student_info(request, contact_id, group_id, message):
-    contact = get_object_or_404(Contact, pk=contact_id)
-    group = get_object_or_404(Group, pk=group_id)
-    return render(request, 'skraning/student_info.html', {'contact': contact, 'group': group, 'message': message})
-
-def next_student_info(request, contact_id, group_id, message):
-    # Bý til nema
-    if request.POST.get('next', False):
-        group_of_student = get_object_or_404(Group, pk=group_id)
-        contact_of_student = get_object_or_404(Contact, pk=contact_id)
-        student = Student(name=request.POST['name'], kt=request.POST['kt'], group=group_of_student)
-        student.save()
-
-        return HttpResponseRedirect(reverse('skraning:student_info', args=[contact_of_student.id, group_of_student.id,'0']))
-
-    elif request.POST.get('finish', False):
-        # Athuga hvort nemandi sé skráður
-        return HttpResponse('Takk fyrir að taka þátt')
-
-    else:
-        return HttpResponse('KLÚÐUR c",')
-
-def finish_enrollment(request):
-    return HttpResponse('Takk fyrir að taka þátt')
-
-
-
-
-
-
-
-
+def enrollment_info(request, message):
+    return render(request, 'skraning/enrollment_info.html', {'message': message})
 
 
 def download_excel(request):
@@ -71,32 +21,36 @@ def download_excel(request):
         return response
     #raise Http404
 
-def read_excel_upload(request, contact_id, group_id):
+def upload_enrollment_info(request):
+    contact = Contact(name=request.POST['name'], email=request.POST['email'])
+    contact.save()
+    group = Group(school=request.POST['school'], grade=request.POST['grade'])
+    group.save()
+    contact.groups.add(group)
 
     if 'skradir_nemendur' in request.FILES:
         file_name = request.FILES['skradir_nemendur'].name
     else:
         message = 2
-        print message
-        return HttpResponseRedirect(reverse('skraning:student_info', args=[contact_id, group_id, message]))
+        return HttpResponseRedirect(reverse('skraning:enrollment_info', args=[message]))
 
 
     if file_name[-4:] == 'xlsx' or file_name[-3:] == 'xls':
         skradir_nemendur = pandas.read_excel(request.FILES['skradir_nemendur'])
-        group_of_student = get_object_or_404(Group, pk=group_id)
+        # group_of_student = get_object_or_404(Group, pk=group.id)
         for i in skradir_nemendur.index:
             student_name = skradir_nemendur.iloc[i,0]
             student_kt = skradir_nemendur.iloc[i,1]
-            student = Student(name=student_name, kt=student_kt, group=group_of_student)
+            student = Student(name=student_name, kt=student_kt, group=group)
             student.save()
-        return HttpResponseRedirect(reverse('skraning:student_table', args=[contact_id, group_id]))
+        return HttpResponseRedirect(reverse('skraning:finish_enrollment', args=[contact.id, group.id]))
     else:
         message = 1
         print message
-        return HttpResponseRedirect(reverse('skraning:student_info', args=[contact_id, group_id, message]))
+        return HttpResponseRedirect(reverse('skraning:enrollment_info', args=[message]))
     return HttpResponse(' c",')
 
-def student_table(request, contact_id, group_id):
+def finish_enrollment(request, contact_id, group_id):
     group = get_object_or_404(Group, pk=group_id)
     student_list = group.student_set.all()
     student_list = student_list.order_by('name')
