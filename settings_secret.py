38d516e95b9854839f303665570cd28b795361a4
Solvi Rognvaldsson <sor16@hi.is>
2017-12-03 13:23:09 +0100

    Still configuring settings for setting up the server

--- settings_secret.py
+++ settings_secret.py
@@ -1,4 +1,7 @@
 import os
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 
@@ -19,7 +22,7 @@ DATABASES = {
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'nemendasvor.pangea@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ["email_password"]
+EMAIL_HOST_PASSWORD = os.environ["gmail_password"]
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
