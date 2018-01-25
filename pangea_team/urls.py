7401ada4e8481a0699de3fe3dc4515d1c26a0217
Gunnar Arthúr Helgason <gunnararthur@Helgis-Air.lan>
2018-01-25 02:50:59 +0100

    Redirect from pangea_team/, css (possible conflicts w email_list commit)

--- pangea_team/urls.py
+++ pangea_team/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 from . import views
 
 
 app_name = 'pangea_team'
 urlpatterns = [
+    url(r'^$', views.home),
     url(r'^login/', auth_views.login, name='login'),
     url(r'^logout/', auth_views.logout, name='logout'),
-    url(r'^home/$', views.home),
-
+    url(r'^home/$', views.home, name='home'),
 
     url(r'^home/email$', views.email_UI, name='email_UI'),
     url(r'^home/send_email$', views.send_email, name='send_email'),
